acf96ecd67e27bb1e9f7042ea31703b928efbfb8
Avihai Shalom <avihais12344@gmail.com>
2022-07-06 18:32:08 +0300

    Added 2 other functions of the same exercise.

--- src/ex-7/main.c
+++ src/ex-7/main.c
@@ -3,6 +3,8 @@
 #include "../ex-3/rectangle.h"  //Rectangle.
 
 #define ARRAY_SIZE 3
+#define NOT_EQUAL 0
+#define EQUAL 1
 
 /**
  * @brief This function copies the first array to the second array.
@@ -15,9 +17,41 @@ void charArrCopy(char* firstArr, char* secondArr, size_t arrSize) {
     }
 }
 
+unsigned char charArrCompare(char* firstArr, 
+                    char* secondArr, 
+                    size_t firstArrSize,
+                    size_t secondArrSize) 
+{
+    // Could use here memcopy, but because we want to learn here, I did it myself.
+    if(firstArrSize != secondArrSize) {
+        return NOT_EQUAL;
+    }
+    for(size_t index = 0; index < firstArrSize; index++) {
+        if(firstArr[index] != secondArr[index]) {
+            return NOT_EQUAL;
+        }
+    }
+    return EQUAL;
+}
+
+void charArrSetValues(char* charArr,
+                    size_t charArrSize,
+                    char valueToSet)
+{
+    // Could use memset here, but because we want to learn here, I did it myself.
+    for(size_t index = 0; index < charArrSize; index++) {
+        charArr[index] = valueToSet;
+    }
+}
+
 int main(void){
     char firstArr[ARRAY_SIZE] = {1};
     char secondArr[ARRAY_SIZE] = {0};
-    charArrCopy(firstArr, secondArr, ARRAY_SIZE);
+    // Original solution.
+    // charArrCopy(firstArr, secondArr, ARRAY_SIZE);
+    // We can also do this.
+    charArrCopy(firstArr, secondArr, sizeof(firstArr));
+    printf("Are the arrays equal: %s", charArrCompare(firstArr, secondArr, ARRAY_SIZE, ARRAY_SIZE) == EQUAL ? "true" : "false");
+    charArrSetValues(secondArr, ARRAY_SIZE, 0);
     return 0;
 }
